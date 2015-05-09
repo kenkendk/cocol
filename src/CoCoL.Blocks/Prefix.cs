97bacaf8695457f943204af0680d8bef2299ff94
Kenneth Skovhede <kenneth@hexad.dk>
2015-05-10 01:04:54 +0200

    Added prefix and successor to the blocks library

---
+++ src/CoCoL.Blocks/Prefix.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CoCoL.Blocks
+{
+	/// <summary>
+	/// The prefix process outputs a value a number of times,
+	/// and then becomes an identity process
+	/// </summary>
+	public class Prefix<T> : BlockBase
+	{
+		private IChannel<T> m_input;
+		private IChannel<T> m_output;
+		private T m_value;
+		private long m_repeat;
+
+		public Prefix(IChannel<T> input, IChannel<T> output, T value, long repeat = 1)
+		{
+			if (input == null)
+				throw new ArgumentNullException("input");
+			if (output == null)
+				throw new ArgumentNullException("output");
+
+			m_input = input;
+			m_output = output;
+			m_value = value;
+			m_repeat = repeat;
+		}
+
+		public async override void Run()
+		{
+			try
+			{
+				while(m_repeat-- > 0)
+					await m_output.WriteAsync(m_value);
+					
+				while (true)
+					await m_output.WriteAsync(await m_input.ReadAsync());
+			}
+			catch (RetiredException)
+			{
+				m_input.Retire();
+				m_output.Retire();
+			}
+		}
+
+	}
+}
+
---
+++ src/CoCoL.Blocks/Successor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CoCoL.Blocks
+{
+	/// <summary>
+	/// Increments the input before sending it to the output.
+	/// Note that this block only supports the long datatype
+	/// </summary>
+	public class Successor : BlockBase
+	{
+		private IChannel<long> m_input;
+		private IChannel<long> m_output;
+
+		public Successor(IChannel<long> input, IChannel<long> output)
+		{
+			if (input == null)
+				throw new ArgumentNullException("input");
+			if (output == null)
+				throw new ArgumentNullException("output");
+
+			m_input = input;
+			m_output = output;
+		}
+
+		public async override void Run()
+		{
+			try
+			{
+				while (true)
+					await m_output.WriteAsync(await m_input.ReadAsync() + 1);
+			}
+			catch (RetiredException)
+			{
+				m_input.Retire();
+				m_output.Retire();
+			}
+		}
+	}
+}
+
