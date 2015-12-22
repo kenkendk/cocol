f72982e12c7240e71dc39c3a9996180835b08508
Kenneth Skovhede <kenneth@hexad.dk>
2015-12-22 18:50:08 +0100

    Added additional timeout tests

--- src/UnitTest/TimeoutTests.cs
+++ src/UnitTest/TimeoutTests.cs
@@ -54,6 +54,41 @@ namespace UnitTest
 				throw new Exception("Failed to get timeout");
 
 		}
+
+		[Test]
+		public void TestTimeoutMultipleTimes()
+		{
+			var c1 = ChannelManager.CreateChannel<int>();
+			var c2 = ChannelManager.CreateChannel<int>();
+			var c3 = ChannelManager.CreateChannel<int>();
+			var c4 = ChannelManager.CreateChannel<int>();
+
+			Func<Task> p = async() =>
+				{
+					try
+					{
+						var t = await Task.WhenAny(
+							c1.ReadAsync(TimeSpan.FromSeconds(5)),
+							c2.ReadAsync(TimeSpan.FromSeconds(4)),
+							c3.ReadAsync(TimeSpan.FromSeconds(2)),
+							c4.ReadAsync(TimeSpan.FromSeconds(4))
+						);
+
+						if (!t.IsFaulted || !(t.Exception.InnerException is TimeoutException))
+							throw new Exception("Timeout did not happen?");
+					}
+					catch (TimeoutException)
+					{
+					}
+				};
+
+			if (!p().Wait(TimeSpan.FromSeconds(3)))
+				throw new Exception("Failed to get timeout");
+
+			if (!p().Wait(TimeSpan.FromSeconds(3)))
+				throw new Exception("Failed to get timeout");
+			
+		}
 	}
 }
 
