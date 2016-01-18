41b305e60e09c23e31891bd1dacac7d1d5373dd4
Kenneth Skovhede <kenneth@hexad.dk>
2016-01-18 16:04:11 +0100

    Added unittests for the autowire code

---
+++ src/UnitTest/AutoWireTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+using CoCoL;
+
+namespace UnitTest
+{
+	[TestFixture]
+	public class AutoWireTests
+	{
+		private class Reader
+		{
+			[ChannelName("input")]
+			private IReadChannel<int> m_read;
+
+			public bool HasChannel { get { return m_read != null; } }
+
+			public bool IsChannelRetired { get { return m_read.IsRetired; } }
+		}
+
+		private class Writer
+		{
+			[ChannelName("input")]
+			private IWriteChannel<int> m_write;
+
+			public bool HasChannel { get { return m_write != null; } }
+
+			public bool IsChannelRetired { get { return m_write.IsRetired; } }
+		}
+
+		[Test]
+		public void SimpleTest()
+		{
+			Reader x1, x2;
+			Writer y;
+			using (new ChannelScope())
+			{
+				AutomationExtensions.AutoWireChannels(new object[] {
+					x1 = new Reader(),
+					x2 = new Reader(),
+					y = new Writer()
+				});
+			}
+
+			if (x1 == null || !x1.HasChannel || x2 == null || !x2.HasChannel)
+				throw new Exception("Autoloader failed to load channel");
+
+			if (ChannelScope.Current != ChannelScope.Root)
+				throw new Exception("Unexpected current scope");
+
+			AutomationExtensions.RetireAllChannels(x1);
+
+			if (x1.IsChannelRetired || y.IsChannelRetired)
+				throw new Exception("Unexpected early retire");
+
+			AutomationExtensions.RetireAllChannels(x2);
+
+			if (!x1.IsChannelRetired || !y.IsChannelRetired)
+				throw new Exception("Unexpected non-retire");
+
+			using (new ChannelScope())
+				AutomationExtensions.AutoWireChannels(y = new Writer());
+
+			if (y == null || !y.HasChannel || y.IsChannelRetired)
+				throw new Exception("Scope does not appear isolated");
+
+		}
+	}
+}
+
---
+++ src/UnitTest/ProcessTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using CoCoL;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+	[TestFixture]
+	public class ProcessTests
+	{
+		[Process(10)]
+		private class Producer : ProcessHelper
+		{
+			[ChannelName("work")]
+			private IWriteChannelEnd<int> m_channel;
+
+			protected override async Task Start()
+			{
+				for (var i = 0; i < 5; i++)
+					await m_channel.WriteAsync(i);
+			}
+		}
+
+		private class Consumer : ProcessHelper
+		{
+			public int Sum { get; private set; }
+			
+			[ChannelName("work")]
+			private IReadChannelEnd<int> m_channel;
+
+			protected override async Task Start()
+			{
+				while (true)
+					Sum += await m_channel.ReadAsync();
+			}
+		}
+
+		[Test]
+		public void TestSimple()
+		{
+			Consumer c;
+			using (new ChannelScope())
+			{
+				Loader.StartFromTypes(typeof(Producer));
+
+				// Auto-load the channels while the scope is active
+				c = new Consumer();
+			}
+
+			c.Run();
+
+			if (c.Sum != 100)
+				throw new Exception(string.Format("Autowire or loader failed, sum was supposed to be {0} but was {1}", 100, c.Sum));
+		}	
+	}
+}
+
