041c2db77c001eff21fe5842d7ab9f2e58e8373d
Kenneth Skovhede <kenneth@hexad.dk>
2016-01-29 23:03:55 +0100

    Added comments for the auto-wire markers

--- src/CoCoL/AutoWireMarkers.cs
+++ src/CoCoL/AutoWireMarkers.cs
@@ -3,13 +3,32 @@ using System.Threading.Tasks;
 
 namespace CoCoL
 {
+	/// <summary>
+	/// Static factory class for creating channel markers
+	/// </summary>
 	public static class ChannelMarker
 	{
+		/// <summary>
+		/// Creates a marker property for a read channel
+		/// </summary>
+		/// <returns>The marker instance.</returns>
+		/// <param name="name">The name of the channel.</param>
+		/// <param name="buffersize">The desired buffersize to use if the channel is created.</param>
+		/// <param name="targetScope">The scope to create or locate the name in.</param>
+		/// <typeparam name="T">The type of data passed on the channel.</typeparam>
 		public static IReadChannel<T> ForRead<T>(string name, int buffersize = 0, ChannelNameScope targetScope = ChannelNameScope.Local)
 		{
 			return new ReadMarker<T>(name, buffersize, targetScope);
 		}
 
+		/// <summary>
+		/// Creates a marker property for a write channel
+		/// </summary>
+		/// <returns>The marker instance.</returns>
+		/// <param name="name">The name of the channel.</param>
+		/// <param name="buffersize">The desired buffersize to use if the channel is created.</param>
+		/// <param name="targetScope">The scope to create or locate the name in.</param>
+		/// <typeparam name="T">The type of data passed on the channel.</typeparam>
 		public static IWriteChannel<T> ForWrite<T>(string name, int buffersize = 0, ChannelNameScope targetScope = ChannelNameScope.Local)
 		{
 			return new WriteMarker<T>(name, buffersize, targetScope);
@@ -21,8 +40,17 @@ namespace CoCoL
 	/// </summary>
 	public abstract class ChannelNameMarker
 	{
+		/// <summary>
+		/// Gets the attribute representation of the data.
+		/// </summary>
 		public ChannelNameAttribute Attribute { get; protected set; }
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CoCoL.ChannelNameMarker"/> class.
+		/// </summary>
+		/// <param name="name">The name of the channel.</param>
+		/// <param name="buffersize">The desired buffersize to use if the channel is created.</param>
+		/// <param name="targetScope">The scope to create or locate the name in.</param>
 		public ChannelNameMarker(string name, int buffersize, ChannelNameScope targetScope)
 		{
 			Attribute = new ChannelNameAttribute(name, buffersize, targetScope);
@@ -34,6 +62,12 @@ namespace CoCoL
 	/// </summary>
 	internal class ReadMarker<T> : ChannelNameMarker, IReadChannel<T>
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CoCoL.ReadMarker`1"/> class.
+		/// </summary>
+		/// <param name="name">The name of the channel.</param>
+		/// <param name="buffersize">The desired buffersize to use if the channel is created.</param>
+		/// <param name="targetScope">The scope to create or locate the name in.</param>
 		public ReadMarker(string name, int buffersize = 0, ChannelNameScope targetScope = ChannelNameScope.Local)
 			: base(name, buffersize, targetScope)
 		{
@@ -43,20 +77,38 @@ namespace CoCoL
 
 
 		#region IReadChannel implementation
+		/// <summary>
+		/// Registers a desire to read from the channel
+		/// </summary>
+		/// <param name="offer">A callback method for offering an item, use null to unconditionally accept</param>
+		/// <param name="timeout">The time to wait for the operation, use zero to return a timeout immediately if no items can be read. Use a
+		/// negative span to wait forever.</param>
+		/// <returns>The async.</returns>
 		public Task<T> ReadAsync(TimeSpan timeout, ITwoPhaseOffer offer = null)
 		{
 			throw new NotImplementedException();
 		}
 		#endregion
 		#region IRetireAbleChannel implementation
+		/// <summary>
+		/// Stops this channel from processing messages
+		/// </summary>
 		public void Retire()
 		{
 			throw new NotImplementedException();
 		}
+		/// <summary>
+		/// Stops this channel from processing messages
+		/// </summary>
+		/// <param name="immediate">Retires the channel without processing the queue, which may cause lost messages</param>
 		public void Retire(bool immediate)
 		{
 			throw new NotImplementedException();
 		}
+		/// <summary>
+		/// Gets a value indicating whether this instance is retired.
+		/// </summary>
+		/// <value><c>true</c> if this instance is retired; otherwise, <c>false</c>.</value>
 		public bool IsRetired
 		{
 			get
@@ -69,6 +121,12 @@ namespace CoCoL
 
 	internal class WriteMarker<T> : ChannelNameMarker, IWriteChannel<T>
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CoCoL.WriteMarker`1"/> class.
+		/// </summary>
+		/// <param name="name">The name of the channel.</param>
+		/// <param name="buffersize">The desired buffersize to use if the channel is created.</param>
+		/// <param name="targetScope">The scope to create or locate the name in.</param>
 		public WriteMarker(string name, int buffersize = 0, ChannelNameScope targetScope = ChannelNameScope.Local)
 			: base(name, buffersize, targetScope)
 		{
@@ -77,20 +135,39 @@ namespace CoCoL
 		// Since this is just a marker, we do not implement any methods
 
 		#region IWriteChannel implementation
+		/// <summary>
+		/// Registers a desire to write to the channel
+		/// </summary>
+		/// <param name="offer">A callback method for offering an item, use null to unconditionally accept</param>
+		/// <param name="value">The value to write to the channel.</param>
+		/// <param name="timeout">The time to wait for the operation, use zero to return a timeout immediately if no items can be read. Use a
+		/// negative span to wait forever.</param>
+		/// <returns>The async.</returns>
 		public Task WriteAsync(T value, TimeSpan timeout, ITwoPhaseOffer offer = null)
 		{
 			throw new NotImplementedException();
 		}
 		#endregion
 		#region IRetireAbleChannel implementation
+		/// <summary>
+		/// Stops this channel from processing messages
+		/// </summary>
 		public void Retire()
 		{
 			throw new NotImplementedException();
 		}
+		/// <summary>
+		/// Stops this channel from processing messages
+		/// </summary>
+		/// <param name="immediate">Retires the channel without processing the queue, which may cause lost messages</param>
 		public void Retire(bool immediate)
 		{
 			throw new NotImplementedException();
 		}
+		/// <summary>
+		/// Gets a value indicating whether this instance is retired.
+		/// </summary>
+		/// <value><c>true</c> if this instance is retired; otherwise, <c>false</c>.</value>
 		public bool IsRetired
 		{
 			get
