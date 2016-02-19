08603f8804a03d23b0c80a97e3258f0f993ee292
Kenneth Skovhede <kenneth@hexad.dk>
2016-02-19 22:13:29 +0100

    Changed CreateChannel and GetChannel methods in ChannelManager to use the current scope

--- src/CoCoL/ChannelManager.cs
+++ src/CoCoL/ChannelManager.cs
@@ -9,27 +9,34 @@ namespace CoCoL
 	public static class ChannelManager
 	{
 		/// <summary>
-		/// Gets a named channel.
+		/// Gets or creates a named channel.
 		/// </summary>
 		/// <returns>The named channel.</returns>
 		/// <param name="name">The name of the channel to find.</param>
 		/// <param name="buffersize">The number of buffers in the channel.</param>
+		/// <param name="scope">The scope to create a named channel in, defaults to null which means the current scope</param>
 		/// <typeparam name="T">The channel type.</typeparam>
-		public static IChannel<T> GetChannel<T>(string name, int buffersize = 0) 
+		public static IChannel<T> GetChannel<T>(string name, int buffersize = 0, ChannelScope scope = null) 
 		{ 
-			return ChannelScope.Root.GetOrCreate<T>(name, buffersize); 
+			return (scope ?? ChannelScope.Current).GetOrCreate<T>(name, buffersize); 
 		}
 
 		/// <summary>
-		/// Creates a channel, possibly unnamed
+		/// Creates a channel, possibly unnamed.
+		/// If a channel name is provided, the channel is created in the supplied scope.
+		/// If a channel with the given name is already found in the supplied scope, the named channel is returned.
 		/// </summary>
 		/// <returns>The channel.</returns>
 		/// <param name="name">The name of the channel, or null.</param>
 		/// <param name="buffersize">The number of buffers in the channel.</param>
+		/// <param name="scope">The scope to create a named channel in, defaults to null which means the current scope</param>
 		/// <typeparam name="T">The channel type.</typeparam>
-		public static IChannel<T> CreateChannel<T>(string name = null, int buffersize = 0) 
-		{ 
-			return new Channel<T>(name, buffersize); 
+		public static IChannel<T> CreateChannel<T>(string name = null, int buffersize = 0, ChannelScope scope = null) 
+		{
+			if (name == null)
+				return CreateChannel<T>(buffersize);
+			else
+				return GetChannel<T>(name, buffersize, scope);
 		}
 
 		/// <summary>
