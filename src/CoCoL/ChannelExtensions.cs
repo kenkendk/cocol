899d66da2540080992a72e8df8474a3e26cf27e2
Dan Stahr <dan.stahr@gmail.com>
2017-08-07 11:08:06 +0100

    Achieve the same ultimate goal while preserving brevity of the original code

--- src/CoCoL/ChannelExtensions.cs
+++ src/CoCoL/ChannelExtensions.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 
 namespace CoCoL
 {
@@ -104,7 +105,15 @@ namespace CoCoL
 				throw new TaskCanceledException();
 			else if (task.IsFaulted)
 			{
-                throw task.Exception;
+                var innerExceptions = task.Exception.Flatten().InnerExceptions;
+                if (innerExceptions.Count == 1)
+                {
+                    ExceptionDispatchInfo.Capture(innerExceptions.First()).Throw();
+                }
+                else
+                {
+                    throw task.Exception;
+                }                
 			}
 
 			return task;
