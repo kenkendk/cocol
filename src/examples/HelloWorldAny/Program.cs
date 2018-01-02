b76fb6831b90b630faa59d29f8c488b36f831890
Kenneth Skovhede <kenneth@hexad.dk>
2018-01-02 14:08:54 +0100

    Added the two `HelloWorld` examples

---
+++ src/examples/HelloWorldAny/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoCoL;
+
+class Example
+{
+    static async Task Produce(IChannel<int> channel)
+    {
+        foreach (var i in Enumerable.Range(0, 5))
+            await channel.WriteAsync(i);
+        channel.Retire();
+    }
+
+    static async Task Consume(IEnumerable<IChannel<int>> channels)
+    {
+        while (true)
+            Console.WriteLine("Hello World: {0}", await channels.ReadFromAnyAsync());
+    }
+
+    static void Main()
+    {
+        var channels = Enumerable.Range(0, 5).Select(x => {
+            var channel = ChannelManager.CreateChannel<int>();
+            Produce(channel);
+            return channel;
+        });
+
+        Consume(channels).Wait();
+    }
+}
