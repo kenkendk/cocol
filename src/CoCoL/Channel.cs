2344688e2d9e9f2defa704756430d1ee6f4fa8f4
Carl Johnsen <carl-johannes@di.ku.dk>
2025-08-13 10:28:25 +0200

    Manual revert of channel since #20

--- src/CoCoL/Channel.cs
+++ src/CoCoL/Channel.cs
@@ -165,11 +165,6 @@ namespace CoCoL
 					EnqueueContinuation
 					? TaskCreationOptions.None
 					: TaskCreationOptions.RunContinuationsAsynchronously);
-				if (offer is ICancelAbleOffer cao)
-				{
-					var src = Source; // Lambda functions doesn't capture class variables.
-					cao.CancelToken.Register(() => src.TrySetCanceled());
-				}
 			}
 
 			/// <summary>
@@ -251,11 +246,6 @@ namespace CoCoL
 					EnqueueContinuation
 					? TaskCreationOptions.None
 					: TaskCreationOptions.RunContinuationsAsynchronously);
-				if (offer is ICancelAbleOffer cao)
-				{
-					var src = Source; // Lambda functions doesn't capture class variables.
-					cao.CancelToken.Register(() => src.TrySetCanceled());
-				}
 				Value = value;
 			}
 
