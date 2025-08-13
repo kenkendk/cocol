0d5c7435a2b628f70cc368f4ea9d7df4dc74e8ca
Carl Johnsen <carl-johannes@di.ku.dk>
2025-08-13 13:14:32 +0200

    Extended the new extension method unit tests to test all combinations of [try]read/write with cancellation tokens

--- src/UnitTest/ChannelExtensionsTest.cs
+++ src/UnitTest/ChannelExtensionsTest.cs
@@ -27,37 +27,95 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public async Task TestReadCanBeCancelled()
-        {
-            var cts = new System.Threading.CancellationTokenSource();
-            var channel = ChannelManager.CreateChannel<int>(buffersize: 0);
-            var task = channel.ReadAsync(cts.Token);
-            cts.Cancel();
-
-            try
-            {
-                await task;
-                Assert.Fail("Exception expected!");
-            }
-            catch (TaskCanceledException)
-            {
-                // Expected result.
-            }
-        }
-
-        [TestMethod]
-        public async Task TestWriteCanBeCancelled()
+        [DataRow(false, false, false)]
+        [DataRow(false, false, true)]
+        [DataRow(false, true, false)]
+        [DataRow(false, true, true)]
+        [DataRow(true, false, false)]
+        [DataRow(true, false, true)]
+        [DataRow(true, true, false)]
+        [DataRow(true, true, true)]
+        public async Task TestCanBeCancelled(bool use_read, bool use_try, bool use_timeout)
         {
             var cts = new System.Threading.CancellationTokenSource();
             var channel = ChannelManager.CreateChannel<int>(buffersize: 0);
 
-            var task = channel.WriteAsync(1, cts.Token);
+            Task task;
+            if (use_read)
+            {
+                if (use_try)
+                {
+                    if (use_timeout)
+                    {
+                        task = channel.TryReadAsync(TimeSpan.FromSeconds(1), cts.Token);
+                    }
+                    else // !use_timeout
+                    {
+                        task = channel.TryReadAsync(cts.Token);
+                    }
+                }
+                else // !use_try
+                {
+                    if (use_timeout)
+                    {
+                        task = channel.ReadAsync(TimeSpan.FromSeconds(1), cts.Token);
+                    }
+                    else // !use_timeout
+                    {
+                        task = channel.ReadAsync(cts.Token);
+                    }
+                }
+            }
+            else // !use_read
+            {
+                if (use_try)
+                {
+                    if (use_timeout)
+                    {
+                        task = channel.TryWriteAsync(1, TimeSpan.FromSeconds(1), cts.Token);
+                    }
+                    else // !use_timeout
+                    {
+                        task = channel.TryWriteAsync(1, cts.Token);
+                    }
+                }
+                else // !use_try
+                {
+                    if (use_timeout)
+                    {
+                        task = channel.WriteAsync(1, TimeSpan.FromSeconds(1), cts.Token);
+                    }
+                    else // !use_timeout
+                    {
+                        task = channel.WriteAsync(1, cts.Token);
+                    }
+                }
+            }
+
             cts.Cancel();
 
             try
             {
-                await task;
-                Assert.Fail("Exception expected!");
+                if (use_try)
+                {
+                    if (use_read)
+                    {
+                        var bt = task as Task<System.Collections.Generic.KeyValuePair<bool, int>>;
+                        var ret = await bt;
+                        Assert.IsFalse(ret.Key, $"Expected false result for Try operation: {use_read}, {use_try}, {use_timeout}");
+                    }
+                    else
+                    {
+                        var bt = task as Task<bool>;
+                        var ret = await bt;
+                        Assert.IsFalse(ret, $"Expected false result for Try operation: {use_read}, {use_try}, {use_timeout}");
+                    }
+                }
+                else
+                {
+                    await task;
+                    Assert.Fail($"Expected TaskCanceledException: {use_read}, {use_try}, {use_timeout}");
+                }
             }
             catch (TaskCanceledException)
             {
